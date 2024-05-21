0bdd9b8d3162f2c1dbb489254abcb58859bac653
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-21 17:46:53 +0700

    menambahkan titik koma pada akhir kurung kurawal

--- Intheritance.cpp
+++ Intheritance.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 class orang {
-    
-}
+public: 
+       string nama;
+
+       orang(sting pNama) :
+           nama(pNama) {
+            cout << "orang dibuat\n" << endl;
+      }
+
+     ~orang() {
+          cout << "orang dihapus\n" << endl;
+     }
+     
+     int jumlah(int a, int b) {
+         return a + b;
+     }
+};
