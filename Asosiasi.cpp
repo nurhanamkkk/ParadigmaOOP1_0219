6af999625f5ac602257156f2aa260066ef6444c7
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-21 17:19:23 +0700

    membuat class dokter

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -3,5 +3,20 @@
 
 class dokter;
 class pasien { // asosiasi
+public:
+       string nama;
+       vector<dokter*> daftar_dokter;
+       pasien(string pNama) :nama(pNama) {
+            cout << "Pasien \"" << nama << "\" ada\n";
+       }
 
+       ~pasien() {
+             cout << "Pasien \"" << nama << "\" tidak ada\n";
+       }
+       void tambahDokter(dokter*);
+       void cetakDokter();
+};
+
+class dokter {
+    
 }
