df8eaafe77ca0e2e5e180e8b91a7d851a690a139
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-21 14:51:41 +0700

    membuat fungsi utama

--- memberinitlist.cpp
+++ memberinitlist.cpp
@@ -7,4 +7,19 @@ private:
         const int id;
         string nama;
         float nilai;
+
+public:
+       mahasiswa(int pId, string pNama, float pNilai)  :id(pId), nama(pNama), nilai (pNilai)  { //Member
+              ///Definisi
+       }
+
+       ~mahasiswa() {
+              cout << "Id   = " << id << endl;
+              cout << "Nama  = " << nama << endl;
+              cout << "Nilai = " << nilai << endl;
+       }
 };
+
+int main() {
+    
+}
